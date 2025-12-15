827e1ee19f2a5907375783c14768386a222296aa
Ryan Almansa <ryan.almansa@gmail.com>
2025-12-15 15:01:09 +0100

    le test et ok

---
+++ tp3.py
@@ -0,0 +1,17 @@
+x = 1 
+        
+if x > 10:
+    print("grand")
+else:
+    print("petit")
+
+note = 15
+
+if note >= 18:
+    print("Excellent")
+elif note >= 14:
+    print("Bien")
+elif note >= 10:
+    print("Passable")
+else:
+    print("Échec")
