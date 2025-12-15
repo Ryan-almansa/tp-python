d4684382f330e21521b0cc4be6919e2fa7b4ca2b
Ryan Almansa <ryan.almansa@gmail.com>
2025-12-15 15:14:16 +0100

    exo 1 version console plus py ok ca marche

--- tp3.py
+++ tp3.py
@@ -1,11 +1,10 @@
-x = 1 
-        
-if x > 10:
-    print("grand")
-else:
-    print("petit")
-
 note = 15
+
+
+if note % 2 == 0:
+    print(f"La note {note} est PAIRE.")
+else:
+    print(f"La note {note} est IMPAIRE.")
 
 if note >= 18:
     print("Excellent")
@@ -15,3 +14,31 @@ elif note >= 10:
     print("Passable")
 else:
     print("Échec")
+
+info = {"Almansa": "Ryan", "19": 30} 
+print(info["Almansa"]) 
+
+#*********************version console**********************
+
+const note = 20;
+
+console.log(`Note : ${note} `);
+
+if (note % 2 === 0) {
+    console.log(`La note ${note} est PAIRE.`);
+} else {
+    console.log(`La note ${note} est IMPAIRE.`);
+}
+
+if (note >= 18) {
+    console.log("Excellent");
+} else if (note >= 14) {
+    console.log("Bien"); 
+} else if (note >= 10) {
+    console.log("Passable");
+} else {
+    console.log("Échec");
+}
+
+const info = {"Almansa": "Ryan", "19": 30}; 
+console.log(info["Almansa"])
