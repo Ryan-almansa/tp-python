7932b160fc4c2ad79e69fdd8c96ecfe073751352
Ryan Almansa <ryan.almansa@gmail.com>
2025-12-15 15:24:22 +0100

    version deux js et python calcule de somme entre deux nombres

--- tp3.py
+++ tp3.py
@@ -1,44 +1,43 @@
-note = 15
+try:
+    N = int(input("Entrez un nombre entier positif : "))
+
+    if N < 1:
+        print("Entre un entier supérieur ou égal à 1.")
+    else:
+        somme_iterative = 0  
+
+       
+        for i in range(1, N + 1):
+            somme_iterative = somme_iterative + i 
+
+        print(f"\nMéthode 1 : Boucle For ")
+        print(f"La somme des nombres de 1 à {N} est : {somme_iterative}")
+
+except ValueError:
+    print("Erreur : s'il te plait entre un nombre entier.")
 
 
-if note % 2 == 0:
-    print(f"La note {note} est PAIRE.")
-else:
-    print(f"La note {note} est IMPAIRE.")
 
-if note >= 18:
-    print("Excellent")
-elif note >= 14:
-    print("Bien")
-elif note >= 10:
-    print("Passable")
-else:
-    print("Échec")
 
-info = {"Almansa": "Ryan", "19": 30} 
-print(info["Almansa"]) 
 
-#*********************version console**********************
-
-const note = 20;
-
-console.log(`Note : ${note} `);
-
-if (note % 2 === 0) {
-    console.log(`La note ${note} est PAIRE.`);
-} else {
-    console.log(`La note ${note} est IMPAIRE.`);
+#************version js**************
+function calculerSommeIterative() {
+    const input = prompt("Entrez un nombre entier positif (N) :");
+    const N = parseInt(input);
+    if (isNaN(N)) {
+        console.error("Erreur : s'il te plait entre un nombre entier.");
+        return;
+    }
+    if (N < 1) {
+        console.log("Entre un entier supérieur ou égal à 1.");
+        return;
+    }
+    let somme_iterative = 0; 
+    for (let i = 1; i <= N; i++) {
+        somme_iterative += i; 
+    }
+    console.log("\nMéthode 1 : Boucle For ");
+    console.log(`La somme des nombres de 1 à ${N} est : ${somme_iterative}`);
 }
 
-if (note >= 18) {
-    console.log("Excellent");
-} else if (note >= 14) {
-    console.log("Bien"); 
-} else if (note >= 10) {
-    console.log("Passable");
-} else {
-    console.log("Échec");
-}
-
-const info = {"Almansa": "Ryan", "19": 30}; 
-console.log(info["Almansa"])
+calculerSommeIterative();
